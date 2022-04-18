87283632fd5b951b178eab47fe67b013ecc27fb8
Asad Saeed <asad.saeed@acidseed.com>
2022-04-18 17:46:23 -0500

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -4,4 +4,4 @@ docker run --rm --privileged \
   -v ~/.docker:/root/.docker \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   homeassistant/amd64-builder \
-  --all -r https://github.com/TD22057/insteon-mqtt -b master
+  --all -r https://github.com/asadpanda/insteon-mqtt -b master
